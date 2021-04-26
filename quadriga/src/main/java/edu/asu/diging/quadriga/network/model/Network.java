918b7729ec03b81b556a0a06b4e913bf2aa1bfea
Aakash Kumar <aakash994@gmail.com>
2021-04-26 01:37:50 -0700

    [Q20-8] network endpoint

---
+++ quadriga/src/main/java/edu/asu/diging/quadriga/network/model/Network.java
@@ -0,0 +1,10 @@
+package edu.asu.diging.quadriga.network.model;
+
+import java.util.List;abstract
+
+public class Network {
+
+    
+    
+    private List<NodeData> defaultMapping; 
+}
---
+++ quadriga/src/main/java/edu/asu/diging/quadriga/network/model/NodeData.java
@@ -0,0 +1,5 @@
+package edu.asu.diging.quadriga.network.model;
+
+public class NodeData {
+
+}
