73c469528091c874ee95ec156c283a382bc155d3
Julia Damerow <julia.damerow@gmail.com>
2021-07-07 16:54:52 -0400

    [Q20-5] formatting

--- quadriga/src/test/java/edu/asu/diging/quadriga/core/service/impl/CollectionManagerImplTest.java
+++ quadriga/src/test/java/edu/asu/diging/quadriga/core/service/impl/CollectionManagerImplTest.java
@@ -13,28 +13,28 @@ import edu.asu.diging.quadriga.core.model.Collection;
 
 public class CollectionManagerImplTest {
 
-	@Mock
-	private CollectionRepository collectionRepo;
+    @Mock
+    private CollectionRepository collectionRepo;
 
-	@InjectMocks
-	private CollectionManagerImpl managerToTest;
+    @InjectMocks
+    private CollectionManagerImpl managerToTest;
 
-	@Before
-	public void setUp() {
-		MockitoAnnotations.initMocks(this);
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
 
-	}
+    }
 
-	@Test
-	public void test_addCollection_success() {
-		Collection collection1 = new Collection();
-		collection1.setName("name");
-		collection1.setDescription("testCollection");
-		Mockito.when(collectionRepo.save(collection1)).thenReturn(collection1);
-		Collection collection=managerToTest.addCollection(collection1.getName(), collection1.getDescription());
-		Mockito.verify(collectionRepo).save(collection);
-		Assert.assertNotNull(collection);
+    @Test
+    public void test_addCollection_success() {
+        Collection collection1 = new Collection();
+        collection1.setName("name");
+        collection1.setDescription("testCollection");
+        Mockito.when(collectionRepo.save(collection1)).thenReturn(collection1);
+        Collection collection = managerToTest.addCollection(collection1.getName(), collection1.getDescription());
+        Mockito.verify(collectionRepo).save(collection);
+        Assert.assertNotNull(collection);
 
-	}
+    }
 
 }
