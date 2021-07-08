9184de62e88e188e9f09c706480647c6bc60928a
Julia Damerow <julia.damerow@gmail.com>
2021-07-08 10:09:39 -0400

    [Q20-5] fixed test case and added id to collections

--- quadriga/src/main/java/edu/asu/diging/quadriga/core/model/Collection.java
+++ quadriga/src/main/java/edu/asu/diging/quadriga/core/model/Collection.java
@@ -1,9 +1,20 @@
 package edu.asu.diging.quadriga.core.model;
 
+import org.bson.types.ObjectId;
+
 public class Collection {
 
+    private ObjectId _id;
     private String name;
     private String description;
+    
+    public ObjectId getId() {
+        return _id;
+    }
+
+    public void setId(ObjectId _id) {
+        this._id = _id;
+    }
 
     public String getName() {
         return name;
--- quadriga/src/test/java/edu/asu/diging/quadriga/core/service/impl/CollectionManagerImplTest.java
+++ quadriga/src/test/java/edu/asu/diging/quadriga/core/service/impl/CollectionManagerImplTest.java
@@ -1,8 +1,10 @@
 package edu.asu.diging.quadriga.core.service.impl;
 
+import org.bson.types.ObjectId;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatcher;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -27,14 +29,26 @@ public class CollectionManagerImplTest {
 
     @Test
     public void test_addCollection_success() {
-        Collection collection1 = new Collection();
-        collection1.setName("name");
-        collection1.setDescription("testCollection");
-        Mockito.when(collectionRepo.save(collection1)).thenReturn(collection1);
-        Collection collection = managerToTest.addCollection(collection1.getName(), collection1.getDescription());
-        Mockito.verify(collectionRepo).save(collection);
-        Assert.assertNotNull(collection);
+        
+        String name = "name";
+        String desc = "description";
+        Collection savedCollection = new Collection();
+        ObjectId id = new ObjectId();
+        savedCollection.setId(id);
+        savedCollection.setName(name);
+        savedCollection.setDescription(desc);
+        Mockito.when(collectionRepo.save(Mockito.argThat(new ArgumentMatcher<Collection>() {
 
+            @Override
+            public boolean matches(Collection arg0) {
+                return (arg0.getName().equals(name) && arg0.getDescription().equals(desc));
+            }
+        }))).thenReturn(savedCollection);
+        
+        Collection collection = managerToTest.addCollection(name, desc);
+        Assert.assertEquals(id, collection.getId());
+        Assert.assertEquals(name, collection.getName());
+        Assert.assertEquals(desc, collection.getDescription());
     }
 
 }
