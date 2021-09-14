816a8b63a3a92664c89aecd17526ed8d9755a905
Julia Damerow <julia.damerow@gmail.com>
2021-09-14 11:10:14 -0400

    added database config and logfile config

--- quadriga/src/main/java/edu/asu/diging/quadriga/config/Neo4jConfig.java
+++ quadriga/src/main/java/edu/asu/diging/quadriga/config/Neo4jConfig.java
@@ -15,10 +15,15 @@ public class Neo4jConfig {
 
     @Value("${neo4j.url}")
     private String neo4jUrl;
+    
+    @Value("${neo4j.database}")
+    private String neo4jDb;
 
     @Bean
     public org.neo4j.ogm.config.Configuration getConfiguration() {
-        return new org.neo4j.ogm.config.Configuration.Builder().uri(neo4jUrl).build();
+        //  there might be  a  bug in Neo4j's HttpDriver? it doesn't seem to take the database.
+        // for now  all data is stored in default db
+        return new org.neo4j.ogm.config.Configuration.Builder().uri(neo4jUrl).database(neo4jDb).build();
     }
 
     @Bean
