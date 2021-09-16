878c104745b1ccc4c2024f39c4cadeee26f59805
Pooja Kulkarni <pakcutie14@gmail.com>
2021-09-16 15:43:29 -0700

    [story/Q20-10] changed RequestParam to PathVariable

--- quadriga/src/main/java/edu/asu/diging/quadriga/web/DeleteCollectionController.java
+++ quadriga/src/main/java/edu/asu/diging/quadriga/web/DeleteCollectionController.java
@@ -2,9 +2,9 @@ package edu.asu.diging.quadriga.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.asu.diging.quadriga.core.exceptions.CollectionNotFoundException;
@@ -16,8 +16,8 @@ public class DeleteCollectionController {
     @Autowired
     private CollectionManager collectionManager;
     
-    @RequestMapping(value = "/auth/collections/delete", method = RequestMethod.GET)
-    public String get(@RequestParam(value = "id", required = true) String id, RedirectAttributes redirectAttributes) {
+    @RequestMapping(value = "/auth/collections/delete/{id}", method = RequestMethod.GET)
+    public String get(@PathVariable String id, RedirectAttributes redirectAttributes) {
         try {
             collectionManager.deleteCollection(id);
             redirectAttributes.addFlashAttribute("alert_type", "success");
