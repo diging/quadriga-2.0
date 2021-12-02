ebd5a60500398661bcbdbe97b10efc74f0e2bd4c
Pooja Kulkarni <pakcutie14@gmail.com>
2021-12-02 16:11:44 -0700

    added test cases for ConceptPower cache and connector services

---
+++ quadriga/src/test/java/edu/asu/diging/quadriga/core/conceptpower/service/impl/ConceptPowerConnectorServiceImplTest.java
@@ -0,0 +1,114 @@
+package edu.asu.diging.quadriga.core.conceptpower.service.impl;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestTemplate;
+
+import edu.asu.diging.quadriga.core.conceptpower.reply.model.ConceptEntry;
+import edu.asu.diging.quadriga.core.conceptpower.reply.model.ConceptPowerReply;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ConceptPowerConnectorServiceImplTest {
+    
+    @Mock
+    private RestTemplate restTemplate;
+    
+    @InjectMocks
+    private ConceptPowerConnectorServiceImpl conceptPowerConnectorServiceImpl;
+    
+    private String conceptPowerBaseUrl;
+    
+    private String conceptPowerIdUrl;
+    
+    private String conceptPowerURL;
+    
+    @Before
+    public void setUp() {
+        conceptPowerBaseUrl = "https://chps.asu.edu/conceptpower";
+        conceptPowerIdUrl = "/rest/Concept?id={concept_uri}";
+        conceptPowerIdUrl = conceptPowerBaseUrl + conceptPowerIdUrl;
+        
+        ReflectionTestUtils.setField(conceptPowerConnectorServiceImpl, "conceptPowerBaseURL", conceptPowerBaseUrl);
+        ReflectionTestUtils.setField(conceptPowerConnectorServiceImpl, "conceptPowerIdURL", conceptPowerIdUrl);
+        
+        MockitoAnnotations.openMocks(this);
+    }
+    
+    
+    @Test
+    public void test_getConceptPowerReply_success() {
+        String conceptUri = "http://www.digitalhps.org/concepts/WID-09972010-N-01-cousin";
+        String lemma = "cousin";
+        
+        Map<String, String> pathVariables = new HashMap<>();
+        pathVariables.put("concept_uri", conceptUri);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
+        
+        ConceptEntry conceptEntry = new ConceptEntry();
+        conceptEntry.setConceptUri(conceptUri);
+        conceptEntry.setLemma(lemma);
+        
+        ConceptPowerReply conceptPowerReply = new ConceptPowerReply();
+        conceptPowerReply.setConceptEntries(Collections.singletonList(conceptEntry));
+        
+        ResponseEntity<ConceptPowerReply> responseEntity = new ResponseEntity<>(conceptPowerReply, HttpStatus.ACCEPTED);
+        
+        Mockito.when(restTemplate.exchange(conceptPowerURL, HttpMethod.GET, httpEntity, ConceptPowerReply.class, pathVariables))
+                .thenReturn(responseEntity);
+        
+        ConceptPowerReply replyFromResponse = conceptPowerConnectorServiceImpl.getConceptPowerReply(conceptUri);
+        
+        Assert.assertNotNull(replyFromResponse.getConceptEntries());
+        Assert.assertEquals(conceptUri, replyFromResponse.getConceptEntries().get(0).getConceptUri());
+        Assert.assertEquals(lemma, replyFromResponse.getConceptEntries().get(0).getLemma());
+    }
+    
+    @Test
+    public void test_getConceptPowerReply_nullResponse() {
+        String conceptUri = "http://www.digitalhps.org/concepts/WID-09972010-N-01-cousin";
+        String lemma = "cousin";
+        
+        Map<String, String> pathVariables = new HashMap<>();
+        pathVariables.put("concept_uri", conceptUri);
+        
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
+        
+        ConceptEntry conceptEntry = new ConceptEntry();
+        conceptEntry.setConceptUri(conceptUri);
+        conceptEntry.setLemma(lemma);
+        
+        ConceptPowerReply conceptPowerReply = new ConceptPowerReply();
+        conceptPowerReply.setConceptEntries(Collections.singletonList(conceptEntry));
+        
+        ResponseEntity<ConceptPowerReply> responseEntity = new ResponseEntity<>(null, HttpStatus.ACCEPTED);
+        
+        Mockito.when(restTemplate.exchange(conceptPowerURL, HttpMethod.GET, httpEntity, ConceptPowerReply.class, pathVariables))
+                .thenReturn(responseEntity);
+        
+        Assert.assertNull(conceptPowerConnectorServiceImpl.getConceptPowerReply(conceptUri));
+    }
+
+}
--- quadriga/src/test/java/edu/asu/diging/quadriga/web/service/impl/GraphCreationServiceImplTest.java
+++ quadriga/src/test/java/edu/asu/diging/quadriga/web/service/impl/GraphCreationServiceImplTest.java
@@ -36,8 +36,8 @@ public class GraphCreationServiceImplTest {
     private GraphCreationServiceImpl graphCreationServiceImpl;
 
     private static List<List<EventGraph>> sampleEventGraphLists = new ArrayList<>();
-    private static ConceptCache subject1;
-    private static ConceptCache predicate1;
+    private static ConceptCache subject1, subject2;
+    private static ConceptCache predicate1, predicate2;
     private static ConceptCache object1;
 
     @BeforeClass
@@ -51,8 +51,9 @@ public class GraphCreationServiceImplTest {
         MockitoAnnotations.openMocks(this);
     }
 
+    
     @Test
-    public void test_createGraph_success_oneSubjectObjectPredicate_oneEventGraph() {
+    public void test_createGraph_singleLevelGraph_success() {
         List<EventGraph> singleEventGraphList = sampleEventGraphLists.get(0);
         EventGraph eventGraph = singleEventGraphList.get(0);
         String[] eventGraphIds = new String[] {eventGraph.getId().toString()};
@@ -82,18 +83,23 @@ public class GraphCreationServiceImplTest {
         Assert.assertEquals(1, subjectNode.getGroup());
         Assert.assertEquals(0, predicateNode.getGroup());
         Assert.assertEquals(1, objectNode.getGroup());
+        
         Assert.assertEquals("Albert Einstein", subjectNode.getLabel());
         Assert.assertEquals("married", predicateNode.getLabel());
         Assert.assertEquals("Elsa Einstein", objectNode.getLabel());
+        
         Assert.assertEquals("Albert Einstein was a German-born theoretical physicist", subjectNode.getDescription());
         Assert.assertEquals("a person who is married; \"we invited several young marrieds\"", predicateNode.getDescription());
         Assert.assertEquals("Elsa Einstein was the second wife and cousin of Albert Einstein", objectNode.getDescription());
+        
         Assert.assertEquals(subjectSourceURI, subjectNode.getUri());
         Assert.assertEquals(predicateSourceURI, predicateNode.getUri());
         Assert.assertEquals(objectSourceURI, objectNode.getUri());
+        
         Assert.assertArrayEquals(eventGraphIds, subjectNode.getEventGraphIds().toArray());
         Assert.assertArrayEquals(eventGraphIds, predicateNode.getEventGraphIds().toArray());
         Assert.assertArrayEquals(eventGraphIds, objectNode.getEventGraphIds().toArray());
+        
         Assert.assertArrayEquals(new String[] {
                 "http://www.digitalhps.org/concepts/albert-einstein",
                 "http://www.digitalhps.org/concepts/albert-einstein-alt"
@@ -107,7 +113,95 @@ public class GraphCreationServiceImplTest {
                 "http://www.digitalhps.org/concepts/elsa-einstein-alt"
                 }, objectNode.getAlternativeUris().toArray());
     }
+    
+    
+    @Test
+    public void test_createGraph_nestedEventGraph_success() {
+        List<EventGraph> nestedEventGraphList = sampleEventGraphLists.get(1);
+        EventGraph eventGraph = nestedEventGraphList.get(0);
+        String[] eventGraphIds = new String[] {eventGraph.getId().toString()};
+        
+        String subjectSourceURI = "http://www.digitalhps.org/concepts/albert-einstein";
+        String predicateSourceURI = "http://www.digitalhps.org/concepts/married";
+        String subjectSourceURI2 = "http://www.digitalhps.org/concepts/cousin";
+        String predicateSourceURI2 = "http://www.digitalhps.org/concepts/be";
+        String objectSourceURI = "http://www.digitalhps.org/concepts/elsa-einstein";
+        
+        Mockito.when(conceptPowerService.getConceptByUri(subjectSourceURI)).thenReturn(subject1);
+        Mockito.when(conceptPowerService.getConceptByUri(predicateSourceURI)).thenReturn(predicate1);
+        Mockito.when(conceptPowerService.getConceptByUri(objectSourceURI)).thenReturn(object1);
+        Mockito.when(conceptPowerService.getConceptByUri(subjectSourceURI2)).thenReturn(subject2);
+        Mockito.when(conceptPowerService.getConceptByUri(predicateSourceURI2)).thenReturn(predicate2);
+        
+        GraphElements graphElements = graphCreationServiceImpl.createGraph(nestedEventGraphList);
+        
+        List<GraphElement> nodes = graphElements.getNodes();
+        List<GraphElement> edges = graphElements.getEdges();
+        
+        Assert.assertEquals(5, nodes.size());
+        Assert.assertEquals(4, edges.size());
+        nodes.forEach(node -> Assert.assertNotNull(node.getData()));
+        edges.forEach(edge -> Assert.assertNotNull(edge.getData()));
+        
+        GraphNodeData predicateNode = (GraphNodeData)nodes.get(0).getData();
+        GraphNodeData subjectNode = (GraphNodeData)nodes.get(1).getData();
+        GraphNodeData predicateNode2 = (GraphNodeData)nodes.get(2).getData();
+        GraphNodeData subjectNode2 = (GraphNodeData)nodes.get(3).getData();
+        GraphNodeData objectNode = (GraphNodeData)nodes.get(4).getData();
+        
+        Assert.assertEquals(1, subjectNode.getGroup());
+        Assert.assertEquals(0, predicateNode.getGroup());
+        Assert.assertEquals(1, objectNode.getGroup());
+        Assert.assertEquals(1, subjectNode2.getGroup());
+        Assert.assertEquals(0, predicateNode2.getGroup());
+        
+        Assert.assertEquals("Albert Einstein", subjectNode.getLabel());
+        Assert.assertEquals("married", predicateNode.getLabel());
+        Assert.assertEquals("cousin", subjectNode2.getLabel());
+        Assert.assertEquals("be", predicateNode2.getLabel());
+        Assert.assertEquals("Elsa Einstein", objectNode.getLabel());
+        
+        Assert.assertEquals("Albert Einstein was a German-born theoretical physicist", subjectNode.getDescription());
+        Assert.assertEquals("a person who is married; \"we invited several young marrieds\"", predicateNode.getDescription());
+        Assert.assertEquals("the child of your aunt or uncle", subjectNode2.getDescription());
+        Assert.assertEquals("be identical to; be someone or something; \"The president of the company is John Smith\"; \"This is my house\"", predicateNode2.getDescription());
+        Assert.assertEquals("Elsa Einstein was the second wife and cousin of Albert Einstein", objectNode.getDescription());
+        
+        Assert.assertEquals(subjectSourceURI, subjectNode.getUri());
+        Assert.assertEquals(predicateSourceURI, predicateNode.getUri());
+        Assert.assertEquals(subjectSourceURI2, subjectNode2.getUri());
+        Assert.assertEquals(predicateSourceURI2, predicateNode2.getUri());
+        Assert.assertEquals(objectSourceURI, objectNode.getUri());
+        
+        Assert.assertArrayEquals(eventGraphIds, subjectNode.getEventGraphIds().toArray());
+        Assert.assertArrayEquals(eventGraphIds, predicateNode.getEventGraphIds().toArray());
+        Assert.assertArrayEquals(eventGraphIds, subjectNode2.getEventGraphIds().toArray());
+        Assert.assertArrayEquals(eventGraphIds, predicateNode2.getEventGraphIds().toArray());
+        Assert.assertArrayEquals(eventGraphIds, objectNode.getEventGraphIds().toArray());
+        
+        Assert.assertArrayEquals(new String[] {
+                "http://www.digitalhps.org/concepts/albert-einstein",
+                "http://www.digitalhps.org/concepts/albert-einstein-alt"
+                }, subjectNode.getAlternativeUris().toArray());
+        Assert.assertArrayEquals(new String[] {
+                "http://www.digitalhps.org/concepts/WID-10295819-N-01-married",
+                "http://www.digitalhps.org/concepts/WID-10295819-N-02-married"
+                }, predicateNode.getAlternativeUris().toArray());
+        Assert.assertArrayEquals(new String[] {
+                "http://www.digitalhps.org/concepts/cousin",
+                "http://www.digitalhps.org/concepts/cousin-2"
+                }, subjectNode2.getAlternativeUris().toArray());
+        Assert.assertArrayEquals(new String[] {
+                "http://www.digitalhps.org/concepts/be",
+                "http://www.digitalhps.org/concepts/be-2"
+                }, predicateNode2.getAlternativeUris().toArray());
+        Assert.assertArrayEquals(new String[] {
+                "http://www.digitalhps.org/concepts/elsa-einstein",
+                "http://www.digitalhps.org/concepts/elsa-einstein-alt"
+                }, objectNode.getAlternativeUris().toArray());
+    }
 
+    
     private static void createSampleEventGraphs(List<List<EventGraph>> sampleEventGraphs) {
 
         RelationEvent rootEvent = new RelationEvent();
@@ -142,9 +236,44 @@ public class GraphCreationServiceImplTest {
 
         EventGraph eventGraph = new EventGraph(rootEvent);
         eventGraph.setId(new ObjectId());
-
+        
         List<EventGraph> singleEventGraphList = Collections.singletonList(eventGraph);
         sampleEventGraphs.add(singleEventGraphList);
+        
+        RelationEvent rootEvent2 = new RelationEvent();
+        RelationEvent object2 = new RelationEvent();
+        Relation baseRelation = new Relation();
+        Relation objectRelation = new Relation();
+        
+        AppellationEvent subject2 = new AppellationEvent();
+        Term termSubject2 = new Term();
+        Concept interpretationSubject2 = new Concept();
+        interpretationSubject2.setSourceURI("http://www.digitalhps.org/concepts/cousin");
+        termSubject2.setInterpretation(interpretationSubject2);
+        subject2.setTerm(termSubject2);
+        
+        AppellationEvent predicate2 = new AppellationEvent();
+        Term termPredicate2 = new Term();
+        Concept interpretationPredicate2 = new Concept();
+        interpretationPredicate2.setSourceURI("http://www.digitalhps.org/concepts/be");
+        termPredicate2.setInterpretation(interpretationPredicate2);
+        predicate2.setTerm(termPredicate2);
+
+        objectRelation.setSubject(subject2);
+        objectRelation.setPredicate(predicate2);
+        objectRelation.setObject(object);
+        object2.setRelation(objectRelation);
+        baseRelation.setSubject(subject);
+        baseRelation.setPredicate(predicate);
+        baseRelation.setObject(object2);
+        rootEvent2.setRelation(baseRelation);
+        
+        EventGraph eventGraph2 = new EventGraph(rootEvent2);
+        eventGraph2.setId(new ObjectId());
+        
+        List<EventGraph> nestedEventGraphList = Collections.singletonList(eventGraph2);
+        sampleEventGraphs.add(nestedEventGraphList);
+        
 
     }
 
@@ -173,6 +302,22 @@ public class GraphCreationServiceImplTest {
         object1.setAlternativeUris(Arrays.asList(
                 "http://www.digitalhps.org/concepts/elsa-einstein",
                 "http://www.digitalhps.org/concepts/elsa-einstein-alt"));
+        
+        subject2 = new ConceptCache();
+        subject2.setUri("http://www.digitalhps.org/concepts/cousin");
+        subject2.setWord("cousin");
+        subject2.setDescription("the child of your aunt or uncle");
+        subject2.setAlternativeUris(Arrays.asList(
+                "http://www.digitalhps.org/concepts/cousin",
+                "http://www.digitalhps.org/concepts/cousin-2"));
+        
+        predicate2 = new ConceptCache();
+        predicate2.setUri("http://www.digitalhps.org/concepts/be");
+        predicate2.setWord("be");
+        predicate2.setDescription("be identical to; be someone or something; \"The president of the company is John Smith\"; \"This is my house\"");
+        predicate2.setAlternativeUris(Arrays.asList(
+                "http://www.digitalhps.org/concepts/be",
+                "http://www.digitalhps.org/concepts/be-2"));
     }
 
 }
