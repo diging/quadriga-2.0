aecbb8ec859bb70971bc8f9379c9b4cf29b42a9a
Prachi Kharge <prachikharge@Prachis-MacBook-Air.local>
2022-03-18 13:54:36 -0700

    [Q20-18] added test case

--- quadriga/src/test/java/edu/asu/diging/quadriga/core/service/impl/EventGraphServiceImplTest.java
+++ quadriga/src/test/java/edu/asu/diging/quadriga/core/service/impl/EventGraphServiceImplTest.java
@@ -1,20 +1,42 @@
 package edu.asu.diging.quadriga.core.service.impl;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockitoSession;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.core.SimpleMongoClientDbFactory;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
 
+import edu.asu.diging.quadriga.api.v1.model.Context;
 import edu.asu.diging.quadriga.core.data.EventGraphRepository;
 import edu.asu.diging.quadriga.core.model.EventGraph;
+
+
 
 public class EventGraphServiceImplTest {
 
@@ -23,6 +45,9 @@ public class EventGraphServiceImplTest {
 
     @InjectMocks
     private EventGraphServiceImpl eventGraphServiceImpl;
+    
+    @Mock
+    MongoTemplate mongoTemplate;
 
     @Before
     public void setUp() {
@@ -60,5 +85,47 @@ public class EventGraphServiceImplTest {
         Assert.assertEquals(eventGraphObjectId1, foundEventGraphs.get(1).getId());
     }
     
+    
+    @Test
+    public void test_groupEventGraphsBySourceUri_success() throws InterruptedException {
+        ObjectId collectionObjectId = new ObjectId();
+
+        EventGraph eventGraph1 = new EventGraph();
+        ObjectId eventGraphObjectId1 = new ObjectId();
+        Context context1= new Context();
+        context1.setSourceUri("uri1");
+
+        eventGraph1.setId(eventGraphObjectId1);
+        eventGraph1.setCollectionId(collectionObjectId);
+        eventGraph1.setContext(context1);
+
+        EventGraph eventGraph2 = new EventGraph();
+        ObjectId eventGraphObjectId2 = new ObjectId();
+        Context context2= new Context();
+        context2.setSourceUri("uri1");
+        eventGraph2.setId(eventGraphObjectId2);
+        eventGraph2.setCollectionId(collectionObjectId);
+        eventGraph2.setContext(context2);
+
+        List<EventGraph> eventGraphs = new ArrayList<EventGraph>();
+        eventGraphs.add(eventGraph2);
+        eventGraphs.add(eventGraph1);
+
+        Document doc1= new Document();
+        doc1.put("total", 1);
+        List<Document> mappedResult= new ArrayList<Document>();
+        mappedResult.add(doc1);
+
+        AggregationResults<Document> aggregationResultsMock = mock(AggregationResults.class);
+                  
+        Mockito.when(aggregationResultsMock.getMappedResults()).thenReturn(mappedResult);
+        Mockito.when(mongoTemplate.getCollectionName(eq(EventGraph.class))).thenReturn("EVENT_GRAPH");
+        Mockito.when(mongoTemplate.aggregate(any(Aggregation.class), any(String.class), eq(Document.class)))
+        .thenReturn(aggregationResultsMock);
+        long totalCount = eventGraphServiceImpl.groupEventGraphsBySourceUri(collectionObjectId);
+
+        //Both the event graphs belong to the group since the sourceUri is same. 
+        Assert.assertEquals(totalCount, 1);
+    }
 
 }
