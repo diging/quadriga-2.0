5d940c87986ec1757e541653674d08ad2aa9e010
Maulik Limbadiya <limbadiyamaulik@gmail.com>
2022-06-22 12:11:19 -0700

    [Q20-15] - Parent conceptpower response class

--- quadriga/src/main/java/edu/asu/diging/quadriga/core/model/conceptpower/ConceptpowerResponse.java
+++ quadriga/src/main/java/edu/asu/diging/quadriga/core/model/conceptpower/ConceptpowerResponse.java
@@ -1,5 +1,17 @@
 package edu.asu.diging.quadriga.core.model.conceptpower;
 
+import java.util.List;
+
 public class ConceptpowerResponse {
+    
+    private List<ConceptEntry> conceptEntries;
+
+    public List<ConceptEntry> getConceptEntries() {
+        return conceptEntries;
+    }
+
+    public void setConceptEntries(List<ConceptEntry> conceptEntries) {
+        this.conceptEntries = conceptEntries;
+    }
 
 }
