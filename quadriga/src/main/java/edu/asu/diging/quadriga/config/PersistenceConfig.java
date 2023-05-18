cea61a13854e9b22797f3b68a45a1ac843cd0c9d
ShivaniYerram <shivaniyerram@gmail.com>
2023-05-17 21:28:38 -0700

    [Q20-22] Removed sending csrf token from controller.

--- quadriga/src/main/java/edu/asu/diging/quadriga/config/PersistenceConfig.java
+++ quadriga/src/main/java/edu/asu/diging/quadriga/config/PersistenceConfig.java
@@ -71,7 +71,7 @@ public class PersistenceConfig {
         Properties properties = new Properties();
         properties.setProperty("hibernate.hbm2ddl.auto", "update");
 
-        properties.setProperty("hibernate.dialect", "${hibarnate.dialect}");
+        properties.setProperty("hibernate.dialect", "${hibernate.dialect}");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.id.new_generator_mappings", "true");
 
