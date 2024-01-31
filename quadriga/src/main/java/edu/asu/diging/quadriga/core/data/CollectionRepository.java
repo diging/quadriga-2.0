2e36f2b482ac88d02debd40664a944e9146102e8
ShivaniYerram <shivaniyerram@gmail.com>
2024-01-31 15:04:15 -0700

    [Q20-22] Resolved the errors and addressed review comments.

--- quadriga/src/main/java/edu/asu/diging/quadriga/core/data/CollectionRepository.java
+++ quadriga/src/main/java/edu/asu/diging/quadriga/core/data/CollectionRepository.java
@@ -3,11 +3,11 @@ package edu.asu.diging.quadriga.core.data;
 import java.util.List;
 
 
+
 import org.bson.types.ObjectId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import edu.asu.diging.quadriga.core.model.Collection;
@@ -15,7 +15,6 @@ import edu.asu.diging.quadriga.core.model.Collection;
 @Repository
 public interface CollectionRepository extends MongoRepository<Collection, ObjectId>{
     
-    @Query("{$or: [{'owner': ?0}, {'apps': {$in: ?1}}]}")
     Page<Collection> findByOwnerOrAppsIn(String owner, List<String> apps, Pageable pageable);
 
     Page<Collection> findByArchived(boolean archived, Pageable paging);
--- quadriga/src/main/java/edu/asu/diging/quadriga/web/ListCollectionController.java
+++ quadriga/src/main/java/edu/asu/diging/quadriga/web/ListCollectionController.java
@@ -1,5 +1,6 @@
 package edu.asu.diging.quadriga.web;
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,8 +56,6 @@ public class ListCollectionController {
                     PageRequest.of(pageInt, sizeInt))); 
             model.addAttribute("username", simpleUser.getUsername());
         }
-        
-        model.addAttribute("collections", collectionManager.findByArchived(false, pageInt,sizeInt));
         return "auth/showcollections";
     }
 
