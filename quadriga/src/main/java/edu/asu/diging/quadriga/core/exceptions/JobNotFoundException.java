5c19c2197197e22aab20e386b30dd88ffbb77689
ShivaniYerram <shivaniyerram@gmail.com>
2024-02-19 19:23:38 -0700

    [Q20-15] Resolved code factor issue.

--- quadriga/src/main/java/edu/asu/diging/quadriga/core/exceptions/JobNotFoundException.java
+++ quadriga/src/main/java/edu/asu/diging/quadriga/core/exceptions/JobNotFoundException.java
@@ -1,23 +1,24 @@
 package edu.asu.diging.quadriga.core.exceptions;
 
 public class JobNotFoundException extends Exception {
+    
 
     private static final long serialVersionUID = -4080713847662867039L;
+    
+    public JobNotFoundException() {
+        super();
+    }
 
-   public JobNotFoundException() {
-       super();
-   }
-   
-   public JobNotFoundException(String exception) {
-       super(exception);
-   }
-   
-   public JobNotFoundException(String message, Throwable cause) {
-       super(message, cause);
-   }
-   
-   public JobNotFoundException(Throwable cause) {
-       super(cause);
-   }
-
+    public JobNotFoundException(String exception) {
+        super(exception);
+    }
+    
+    public JobNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    public JobNotFoundException(Throwable cause) {
+        super(cause);
+    }
+       
 }
