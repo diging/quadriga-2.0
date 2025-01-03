8ad16e997298799aafe7e062c9e75bbc68bf4e6c
pooja-thalur <poojathalur@yahoo.com>
2025-01-03 16:27:24 -0700

    [Q20-25] jcache changes

--- quadriga/src/main/java/edu/asu/diging/quadriga/config/RootConfig.java
+++ quadriga/src/main/java/edu/asu/diging/quadriga/config/RootConfig.java
@@ -10,6 +10,7 @@ import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -26,17 +27,13 @@ public class RootConfig {
 
     @Bean
     public CacheManager cacheManager() throws URISyntaxException {
-    	// Create an Ehcache CacheManager
-        org.ehcache.CacheManager ehCacheManager = CacheManagerBuilder.newCacheManagerBuilder()
-                .withCache("preConfigured", CacheConfigurationBuilder.newCacheConfigurationBuilder(
-                        Long.class,
-                        String.class,
-                        ResourcePoolsBuilder.heap(10)
-                ))
-                .build(true); // Build and initialize
-
-        // Wrap EhCacheManager into Spring's EhCacheCacheManager
-        return new EhCacheCacheManager(ehCacheManager);
+        CacheManagerBuilder.newCacheManagerBuilder().withCache("preConfigured", CacheConfigurationBuilder
+                .newCacheConfigurationBuilder(Long.class, String.class, ResourcePoolsBuilder.heap(10))).build();
+        CachingProvider cachingProvider = Caching.getCachingProvider();
+        javax.cache.CacheManager manager = cachingProvider
+                .getCacheManager(getClass().getResource("/ehcache.xml").toURI(), getClass().getClassLoader());
+        JCacheCacheManager cacheManager = new JCacheCacheManager(manager);
+        return cacheManager;
     }
     
     @Bean
