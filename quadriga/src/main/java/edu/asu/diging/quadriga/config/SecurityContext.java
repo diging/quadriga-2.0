980f577709498287e19b09ce4676f992a8473798
pooja-thalur <poojathalur@yahoo.com>
2025-02-28 16:37:26 -0700

    [Q20-25] fixing login issues

--- quadriga/src/main/java/edu/asu/diging/quadriga/config/SecurityContext.java
+++ quadriga/src/main/java/edu/asu/diging/quadriga/config/SecurityContext.java
@@ -1,23 +1,22 @@
 package edu.asu.diging.quadriga.config;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -38,33 +37,31 @@ public class SecurityContext {
         @Autowired
         private UserDetailsService userManager;
         
-        @Autowired
-        private AuthenticationManagerBuilder builder;
-        
         @Bean
-        public AuthenticationManager authenticationManager(UserDetailsService userManager, BCryptPasswordEncoder passwordEncoder) throws Exception {
-            DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-            authProvider.setUserDetailsService(userManager);
-            return new ProviderManager(authProvider);
-        }
+        InMemoryUserDetailsManager userDetailsService() {
+            UserDetails user = User.withDefaultPasswordEncoder()
+              .username("Admin")
+              .password("admin")
+              .roles("ROLE_ADMIN")
+              .roles("ROLE_USER")
+              .build();
 
-        public void configure(AuthenticationManagerBuilder builder)
-                throws Exception {
-            builder.userDetailsService(userManager);
+            return new InMemoryUserDetailsManager(user);
         }
         
         @Bean
-        public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
-            // Spring Security ignores request to static resources such as CSS or JS
-            // files.
-            return (web) -> web.ignoring().requestMatchers("/static/**");           
+        public WebSecurityCustomizer webSecurityCustomizer() throws Exception {           
+                    // Spring Security ignores request to static resources such as CSS or JS
+                    // files.
+            return (web)-> web.ignoring().requestMatchers("/static/**");
         }
-    
+        
         @Bean
-        protected void configure(HttpSecurity http) throws Exception {
-            HeadersConfigurer<HttpSecurity> config = http.cors().and().csrf()
+        SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.cors().and().authorizeHttpRequests()
+            .requestMatchers("**");
+            HeadersConfigurer<HttpSecurity> config = http.csrf()
                     .requireCsrfProtectionMatcher(new RequestMatcher() {
-                        
                         @Override
                         public boolean matches(HttpServletRequest arg0) {
                             // don't require CSRF for REST calls
@@ -75,7 +72,7 @@ public class SecurityContext {
                                 return false;
                             }
                             return true;
-                        }                   
+                        }
                     }).and().headers().frameOptions().sameOrigin();
              
             config.and().formLogin().loginPage("/login").loginProcessingUrl("/login/authenticate").failureUrl("/loginFailed").and()
@@ -94,6 +91,7 @@ public class SecurityContext {
                     .requestMatchers("/users/**", "/admin/**").hasRole("ADMIN")
                     .requestMatchers("/auth/**").hasAnyRole("USER", "ADMIN")
                     .requestMatchers("/password/**").hasRole(SimpleUsersConstants.CHANGE_PASSWORD_ROLE);
+            return http.build();
         }
     
         @Bean
@@ -106,36 +104,23 @@ public class SecurityContext {
     @Configuration
     @Order(1)
     public class ApiV1WebSecurityConfig {
-        
-        @Autowired
-        private UserDetailsService userManager;
-        
-        @Autowired
-        private AuthenticationConfiguration authenticationConfiguration;
-        
+
         @Bean
         SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
             CitesphereTokenFilter citesphereTokenFilter = new CitesphereTokenFilter("/api/v1/**");
-            citesphereTokenFilter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+            citesphereTokenFilter.setAuthenticationManager(authenticationManager());
             
-            httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-            httpSecurity.authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/v1/**"));
-            httpSecurity.addFilterBefore(citesphereTokenFilter, BasicAuthenticationFilter.class).csrf().disable();
-            return httpSecurity.build();
+            httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                    .authorizeHttpRequests().requestMatchers("/api/v1/**").permitAll().and()
+                    .addFilterBefore(citesphereTokenFilter, BasicAuthenticationFilter.class)
+                    .csrf().disable();
         }
         
         @Bean
         public CitesphereAuthenticationProvider authenticationProvider() {
             return new CitesphereAuthenticationProvider();
         }
-        
-        @Bean
-        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-            return authenticationConfiguration.getAuthenticationManager();
-        }
 
     }
-    
-    
 
 }
