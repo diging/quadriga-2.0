f48182863468530e542f8590372ef1369cc33b4c
pooja-thalur <poojathalur@yahoo.com>
2025-05-07 16:29:00 -0700

    [Q20-16] review comments addressed

--- quadriga/src/main/java/edu/asu/diging/quadriga/core/service/impl/MappedTripleGroupServiceImpl.java
+++ quadriga/src/main/java/edu/asu/diging/quadriga/core/service/impl/MappedTripleGroupServiceImpl.java
@@ -152,12 +152,7 @@ public class MappedTripleGroupServiceImpl implements MappedTripleGroupService {
         // In case this is a new collection, or existing collection but new mapping type for that collection
         // we create a new MappedTripleGroup entry
         if (mappedTripleGroup == null) {
-            mappedTripleGroup = add(collectionId, mappedTripleType);
-
-            // If mappedTripleGroup add fails
-            if (mappedTripleGroup == null) {
-                throw new MappedTripleGroupNotFoundException("Couldn't find or persist a new MappedTripleGroup entry for collectionId: " + collectionId);
-            }
+            mappedTripleGroup = add(collectionId, mappedTripleType);       
         }
         return mappedTripleGroup;
     }
