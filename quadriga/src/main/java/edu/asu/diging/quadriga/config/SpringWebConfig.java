97eadf279b681d42fac9b59e2dc5a597dbd7f5f1
Julian Ophals <jophals@asu.edu>
2025-06-18 14:55:49 -0700

    [story/Q20-28] implemented custom Jackson serializer to handle kepping id as one string value instead of splitting

--- quadriga/src/main/java/edu/asu/diging/quadriga/config/SpringWebConfig.java
+++ quadriga/src/main/java/edu/asu/diging/quadriga/config/SpringWebConfig.java
@@ -1,8 +1,14 @@
 package edu.asu.diging.quadriga.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -11,5 +17,15 @@ public class SpringWebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+    
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        for (HttpMessageConverter<?> converter : converters) {
+            if (converter instanceof MappingJackson2HttpMessageConverter jsonConverter) {
+                ObjectMapper mapper = jsonConverter.getObjectMapper();
+                mapper.registerModule(new ObjectIdToStringModule());
+            }
+        }
     }
 }
