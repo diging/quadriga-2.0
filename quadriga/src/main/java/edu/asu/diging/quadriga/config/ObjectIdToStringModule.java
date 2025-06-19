9a53b8c60c27eceaddaf01353e60c5f857da3ab9
Julian Ophals <jophals@asu.edu>
2025-06-18 23:24:03 -0700

    [story/Q20-24] implemented customized Jackson serializer to fix getCollections response id field

---
+++ quadriga/src/main/java/edu/asu/diging/quadriga/config/ObjectIdToStringModule.java
@@ -0,0 +1,12 @@
+package edu.asu.diging.quadriga.config;
+
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.bson.types.ObjectId;
+
+public class ObjectIdToStringModule extends SimpleModule {
+    public ObjectIdToStringModule() {
+        addSerializer(ObjectId.class, new ToStringSerializer());
+    }
+}
+
